51a6ba91266e106693df5fa1679fee37f7d750f4
iamsuryakant <suryakantthakur825@gmail.com>
2024-05-02 11:54:59 +0530

    feat: added database & cloudinary support

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,12 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
-// cloudinary.config({
-//   cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`,
-//   api_key: `${process.env.CLOUDINARY_API_KEY}`,
-//   api_secret: `${process.env.CLOUDINARY_API_SECRET}`,
-// });
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -21,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-      // file has been uploaded successfull
+    
+    // file has been uploaded successfull
     
     fs.unlinkSync(localFilePath);
     return response;
